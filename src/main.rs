4b9d7bfb8a30866db65b5d2e81ff376d61c71483
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-12-15 19:18:37 +0000

    constants file for ease of tracking frames

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod manjoo;
 mod tomato;
+mod constants;
 use manjoo::Manjoo;
 use ratatui::{
-    backend::TermwizBackend,
-    symbols::Marker,
-    widgets::{canvas::Canvas, Block},
-    Terminal,
+    backend::TermwizBackend, layout::Alignment, symbols::Marker, widgets::{canvas::Canvas, Block}, Terminal
 };
 use std::{
     error::Error,
@@ -20,17 +18,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     terminal.hide_cursor()?;
 
     terminal.clear()?;
+    
     let now = Instant::now();
     let mut sleep_duration = 10;
     let mut render_static = true;
     let mut running_manjoo_x: usize = 0;
     let mut running_flag = false;
     let mut has_tomato = false;
-    let mut gets_tomato = 0;
     let tomato_radius = 6.0;
     let mut size_global = ratatui::layout::Rect::default();
 
-    while now.elapsed() < Duration::from_secs(20) {
+    while now.elapsed() < Duration::from_secs(17) {
         terminal.draw(|f| {
             let size = f.area();
             size_global = size.clone();
@@ -38,7 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .block(
                     Block::default()
                         .borders(ratatui::widgets::Borders::ALL)
-                        .title("WheekHigh, RIP MANJOO"),
+                        .title("WheekHigh, RIP MANJOO").title_alignment(Alignment::Center),
                 )
                 .x_bounds([0.0, (size.width * 2) as f64])
                 .y_bounds([0.0, (size.height * 2) as f64])
@@ -84,7 +82,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             let tomato_x = (size_global.width as f64 / 2.0) as usize - (tomato_radius as usize) * 8;
             if running_manjoo_x >= (tomato_x) {
                 sleep_duration = 100;
-                gets_tomato = running_manjoo_x;
                 has_tomato = true;
             }
         }
